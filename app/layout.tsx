2609f0a746c980d737826b54c7683b9a9378962a
Yoeza Sapoetro <yoezasapoetro@gmail.com>
2023-06-06 00:35:37 +0700

    refactor: remove clerk implementation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,15 @@
 import "@/styles/globals.css"
-import { ClerkProvider } from "@clerk/nextjs"
 import { font } from "@/lib/utils/fonts"
-import ClerkOptions from '@/lib/utils/clerkOptions'
 import { Providers } from "./providers"
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <ClerkProvider {...ClerkOptions}>
-            <html lang="en">
-                <body className={font.className}>
-                    <Providers>
-                        {children}
-                    </Providers>
-                </body>
-            </html>
-        </ClerkProvider>
+        <html lang="en">
+            <body className={font.className}>
+                <Providers>
+                    {children}
+                </Providers>
+            </body>
+        </html>
     )
 }
