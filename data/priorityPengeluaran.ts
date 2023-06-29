8cdebf3efd050f8fcab87e5208c2b8c3eb01bc96
Yoeza Sapoetro <yoezasapoetro@gmail.com>
2023-06-29 13:23:12 +0700

    refactor: migration and dummy data

---
+++ data/priorityPengeluaran.ts
@@ -0,0 +1,31 @@
+import type { IconType } from "react-icons"
+
+import {
+    MdFiberManualRecord as LowPriority,
+    MdRadioButtonChecked as MediumPriority,
+    MdFiberSmartRecord as HighPriority,
+} from "react-icons/md"
+
+import { priorityColors } from "@/components/colors"
+import { PrioritasPengeluaranData } from "@/types/KategoriPengeluaran"
+
+export const prioritasPengeluaranData: Array<PrioritasPengeluaranData> = [
+    {
+        label: 1,
+        labelText: "Prioritas rendah",
+        icon: LowPriority,
+        iconColor: priorityColors.low,
+    },
+    {
+        label: 2,
+        labelText: "Prioritas medium",
+        icon: MediumPriority,
+        iconColor: priorityColors.medium,
+    }, {
+        label: 3,
+        labelText: "Prioritas tinggi",
+        icon: HighPriority,
+        iconColor: priorityColors.high,
+    }
+]
+
---
+++ db/migrations/pg/0004_cool_newton_destine.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "Category" ADD COLUMN "priority" integer DEFAULT 0;
