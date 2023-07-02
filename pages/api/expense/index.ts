0ad51b3bd9c0e594e15de568a2b290b4feaa1338
Yoeza Sapoetro <yoezasapoetro@gmail.com>
2023-07-02 22:12:55 +0700

    feat(api): expense api

---
+++ pages/api/expense/index.ts
@@ -0,0 +1,29 @@
+import { NextApiRequest, NextApiResponse } from "next"
+import { getServerSession } from "next-auth"
+import { getToken } from "next-auth/jwt"
+
+import { authOptions } from "../auth/[...nextauth]"
+import ExpenseService from "@/service/ExpenseService"
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const session = await getServerSession(req, res, authOptions)
+    const token = await getToken({ req })
+
+    if (!session || !token) {
+        res.status(401).json({ message: "unauthorized" })
+        return
+    }
+
+    const userId = token?.sub as string
+
+    const service = new ExpenseService(userId)
+
+    if (req.method === "GET") {
+        return await service.getAllByMonthHandler(req, res)
+    }
+
+    if (req.method === "POST") {
+        return await service.createHandler(req, res)
+    }
+}
+
