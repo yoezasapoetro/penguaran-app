e1e2a705f704f7d097cc49488ecc89260ead9f85
Yoeza Sapoetro <yoezasapoetro@gmail.com>
2023-07-04 13:35:53 +0700

    feat: next pwa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
+const withPwa = require("next-pwa")({
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+module.exports = withPwa(nextConfig);
 
