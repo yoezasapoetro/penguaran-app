3042e4e4b9b86afb7d9a2ea1172bd6b4116dd95c
Yoeza Sapoetro <yoezasapoetro@gmail.com>
2023-07-23 02:13:13 +0700

    feat: pwa initials

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,6 @@ export { default } from "next-auth/middleware"
 
 export const config = {
     matcher: [
-        '/((?!api/auth/:path*|login|logout|_next/static|_next/image|favicon.ico|sw.js|workbok-*.js|manifest.json|icons/*.png).*)'
+        '/((?!api/auth/:path*|login|logout|_next/static|_next/image|favicon.ico|sw.js|workbok-*.js|static/manifest.json|static/icons/*.png).*)'
     ],
 }
